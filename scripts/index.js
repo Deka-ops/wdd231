26001b7ad5b33b899b49e3cddbb4587cf5b96262
Deka-ops <gnwabuikwuchori@byupathway.edu>
2025-05-30 21:19:10 +0100

    updated index

--- scripts/index.js
+++ scripts/index.js
@@ -25,3 +25,26 @@ document.getElementById("menu").addEventListener("click", function () {
 document.querySelector(".close-menu").addEventListener("click", function () {
     document.getElementById("animatemenu").classList.remove("open");
 });
+
+function displayCourseDetails(course) {
+    CourseDetails.innerHTML = '';
+    courseDetails.innerHTML = `
+    <button id="closeModal">X</button>
+    <h2>${course.subject}${course.number}</h2>
+    <h3>${course.title}</h3>
+    <p><strong>Credits</strong>: ${course.credits}</p>
+    <p><strong>Certificate</strong>: ${course.certificate}</p>
+    <p>${course.description}</p>
+    <p><strong>Technologies</strong>: ${course.tecnology.join(', ')}</p>
+    `;
+    courseDetails.showModal();
+
+    closeModal.addEventListener("click", () => {
+        courseDetails.close();
+    })
+
+}
+
+courseDiv.addEventListener('click', () => {
+    displayCourseDetails(course);
+});
